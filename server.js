5fe9ff9fa2abb9248fdc25e36a5d0a0850a1b64d
isabel <isargp@gmail.com>
2022-02-28 10:13:50 +0000

    improved and styled index.htm

--- server.js
+++ server.js
@@ -6,7 +6,8 @@ app.use(express.json());
 app.set('view engine', 'ejs');
 app.use(cors());
 
-const htmlIndexRoute = "C:\\Users\\Isabel\\Documents\\FUTUREPROOF\\JS\\API\\My API\\template\\index.htm"
+const htmlIndexRoute = "C:\\Users\\Isabel\\Documents\\FUTUREPROOF\\JS\\API\\My API\\templates\\index.htm"
+const cssRoute = "C:\\Users\\Isabel\\Documents\\FUTUREPROOF\\JS\\API\\My API\\templates\\styles.css"
 
 const parkRoutes = require('./controllers/parks');
 app.use('/parks', parkRoutes);
@@ -14,6 +15,10 @@ app.use('/parks', parkRoutes);
 app.get('/', (req, res) => {
    res.sendFile(htmlIndexRoute);
 });
+
+app.get('/styles.css', (req, res) => {
+   res.sendFile(cssRoute);
+ });
 
 app.post('/', (req, res) => {
    res.status(405).send('Not allowed!');
